b5bb318bfdb7ac01f56af19a115a6d0ab6d7afde
Caproner <634768901@qq.com>
2019-07-14 17:59:50 +0800

    update to 1_1_1

---
+++ Chapter_1/1_1/1_1_1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main()
+{
+	int x = 5;
+	int y = 10;
+	
+	for(int i = 0; i < x; i++)
+		printf("\n");
+	for(int j = 0; j < y; j++)
+		printf(" ");
+	printf("o\n");
+	
+	return 0;
+}
